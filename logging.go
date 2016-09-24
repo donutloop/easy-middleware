6680b913f7e6c7baba01a0fc48e98bcc8b63b812
Marcel Franke <info@marcel-edmund-franke.de>
2016-09-24 20:45:20 +0200

    Added logging middlware

---
+++ logging.go
@@ -0,0 +1,20 @@
+package easy_middlware
+
+import (
+	"net/http"
+	"time"
+	"log"
+)
+
+// Logging of device request time
+func logging(logger *log.Logger, h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		start := time.Now()
+		logger.Printf("Started %s %s", r.Method, r.URL.Path)
+
+		h.ServeHTTP(w, r)
+
+		logger.Printf("%s Completed in %v", r.UserAgent() ,time.Since(start))
+	})
+}
+
