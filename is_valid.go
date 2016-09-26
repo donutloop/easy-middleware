8f79c1675415aa1b04d578e56bc396424d349e8a
Marcel Franke <info@marcel-edmund-franke.de>
2016-09-26 19:58:12 +0200

    - Added IsVaild Middleware

---
+++ is_valid.go
@@ -0,0 +1,25 @@
+package easy_middlware
+
+import (
+	"net/http"
+)
+
+type validator interface {
+	ok(w http.ResponseWriter, r http.Request) (bool, error)
+}
+
+// Logging of device request time
+func isValid(v validator) Middleware {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+
+			if ok, err := v.ok(w, r); !ok {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+
+			h.ServeHTTP(w, r)
+		})
+	}
+}
+
